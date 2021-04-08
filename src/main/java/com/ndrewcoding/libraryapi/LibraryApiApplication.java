04b85ec29dac08091420d898963f2ae12db7348a
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-04-07 23:27:46 -0300

    project added to heroku and configured to be packaged as a war

--- src/main/java/com/ndrewcoding/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/ndrewcoding/libraryapi/LibraryApiApplication.java
@@ -3,12 +3,13 @@ package com.ndrewcoding.libraryapi;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class LibraryApiApplication {
+public class LibraryApiApplication extends SpringBootServletInitializer {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
